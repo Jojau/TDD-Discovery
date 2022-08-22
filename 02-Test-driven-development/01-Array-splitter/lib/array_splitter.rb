47f8d504501d00731417b1183a8826bda9f60c9f
BUKAL JOHANA <johana.bukal@etu.unistra.fr>
2022-08-22 15:04:56 +0200

    Exercice TDD: Array splitter

--- 02-Test-driven-development/01-Array-splitter/lib/array_splitter.rb
+++ 02-Test-driven-development/01-Array-splitter/lib/array_splitter.rb
@@ -1,1 +1,9 @@
 # TO DO: Write a method that splits an array into 2 different ones.
+def array_splitter(array, index)
+    unless index>array.length
+        first_array = array.slice(0, index)
+        second_array = array.slice(index, array.length)
+        return [first_array, second_array]
+    end
+    "error"
+end
--- 02-Test-driven-development/01-Array-splitter/spec/array_splitter_spec.rb
+++ 02-Test-driven-development/01-Array-splitter/spec/array_splitter_spec.rb
@@ -1,2 +1,19 @@
 # Remember: we are doing TDD here, so don’t rush in the program.
 # You need to write the tests first.
+
+require 'array_splitter'
+array=[1, 2, 3, 4, 5]
+
+describe '#array_splitter' do
+    it 'returns an array when you pass correct arguments' do
+        expect(array_splitter(array, 2)).to be_a(Array)
+    end
+
+    it 'return an array containing our 2 new arrays' do
+        expect(array_splitter(array, 2)).to eq([[1,2], [3,4,5]])
+    end
+
+    it 'return an error when you pass a index greater than the length of the array' do
+        expect(array_splitter(array, array.length+9)).to eq('error')
+    end
+end
