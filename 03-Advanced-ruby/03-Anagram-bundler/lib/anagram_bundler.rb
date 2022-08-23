64f552fa9dd7ee7d969521dbbcc67c46051ac259
BUKAL JOHANA <johana.bukal@etu.unistra.fr>
2022-08-23 12:14:34 +0200

    anagram_bundler marche pas parce que pour moi l'exemple est mauvais

--- 03-Advanced-ruby/03-Anagram-bundler/lib/anagram_bundler.rb
+++ 03-Advanced-ruby/03-Anagram-bundler/lib/anagram_bundler.rb
@@ -1,1 +1,4 @@
 # TO DO: Write a method that groups anagrams together.
+def anagram_bundler(array)
+    groups = array.group_by {|word| word.chars.sort}.values
+end
--- 03-Advanced-ruby/03-Anagram-bundler/spec/anagram_bundler_spec.rb
+++ 03-Advanced-ruby/03-Anagram-bundler/spec/anagram_bundler_spec.rb
@@ -1,1 +1,8 @@
 # TO DO: Write specs for your anagram_bundler method
+require 'anagram_bundler'
+
+describe '#anagram_bundler' do
+    it "takes an array of strings as an arguments, and hould return an array of arrays, each being a group of anagrams" do
+        expect(anagram_bundler(['chien', 'romain', 'niche', 'manoir', 'marino', 'mairie', 'coq', 'aimer', 'plume'])).to eq([['chien', 'niche'],  ['romain', 'manoir', 'marino'], ['mairie', 'aimer'], ['coq'], ['plume']])
+    end
+end
