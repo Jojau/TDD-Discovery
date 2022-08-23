3fa36723026d09c946ef6dfa4c7349f0da36b7ef
BUKAL JOHANA <johana.bukal@etu.unistra.fr>
2022-08-23 13:54:05 +0200

    hey_oh

--- 02-Test-driven-development/03-Hey-oh/lib/hey_oh.rb
+++ 02-Test-driven-development/03-Hey-oh/lib/hey_oh.rb
@@ -1,1 +1,20 @@
 # TO DO: Write a method that takes an array and modify it according to the rules.
+
+def hey_oh(array)
+    if array.is_a? Array
+        new_array = array.map do |value|
+            if value%3==0 && value%5==0
+                "HeyOh"  
+            elsif value%3==0
+                "Hey"
+            elsif value%5==0
+                "Oh"
+            else
+                value
+            end
+        end
+        return new_array
+    else
+        return "error"
+    end
+end
