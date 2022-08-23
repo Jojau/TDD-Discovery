46baad30d7a36a68b5a80799215bc1473010a63f
BUKAL JOHANA <johana.bukal@etu.unistra.fr>
2022-08-23 10:07:12 +0200

    Array converter

--- 03-Advanced-ruby/02-Manipulate-arrays/lib/manipulate_arrays.rb
+++ 03-Advanced-ruby/02-Manipulate-arrays/lib/manipulate_arrays.rb
@@ -1,2 +1,9 @@
 # TO DO: Write the array_converter, reverse_array, starts_with
 # and starts_with_count methods, according to the specs.
+
+def array_converter(array)
+    # Initialising an array and calling each_with_index function
+    array.each_with_index do |value, index|
+        array[index]=(index+1).to_s+' - '+value
+    end
+end
--- 03-Advanced-ruby/02-Manipulate-arrays/spec/manipulate_arrays_spec.rb
+++ 03-Advanced-ruby/02-Manipulate-arrays/spec/manipulate_arrays_spec.rb
@@ -1,2 +1,11 @@
 # TO DO: Write the specs for the array_converter, reverse_array, starts_with
 # and starts_with_count methods.
+
+require 'manipulate_arrays'
+array=['Neil', 'Buzz', 'Michael']
+
+describe '#array_converter' do
+    it "takes an array of strings as an argument and returns it by adding the indexes at the beginning of each element" do
+        expect(array_converter(array)).to eq(['1 - Neil', '2 - Buzz', '3 - Michael'])
+    end
+end
