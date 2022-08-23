7ba7eb5db9e682e96fa712f3a90e2f0d2d0c29b9
BUKAL JOHANA <johana.bukal@etu.unistra.fr>
2022-08-23 09:44:38 +0200

    Test hey oh

--- 02-Test-driven-development/03-Hey-oh/spec/hey_oh_spec.rb
+++ 02-Test-driven-development/03-Hey-oh/spec/hey_oh_spec.rb
@@ -1,1 +1,15 @@
 # TO DO: Write specs for you hey_oh method
+
+require 'hey_oh'
+array=(0..20).to_a
+
+describe '#hey_oh' do
+
+    it 'return an array containing Hey or Oh or HeyOh bref' do
+        expect(hey_oh(array)).to eq([0,1,2,'Hey',4,'Oh','Hey',7,8,'Hey','Oh',11,'Hey',13,14,'HeyOh',16,17,'Hey',19,'Oh'])
+    end
+
+    it "return an error when you don't pass an array" do
+        expect(hey_oh('pouet')).to eq('error')
+    end
+end
