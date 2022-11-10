bdc97730f6f4025b06ff3000c3718e9542438e45
BUKAL JOHANA <johana.bukal@etu.unistra.fr>
2022-11-10 18:13:15 +0100

    jsp ??

--- 03-Advanced-ruby/04-Reverse-roman-style/lib/reverse_roman_style.rb
+++ 03-Advanced-ruby/04-Reverse-roman-style/lib/reverse_roman_style.rb
@@ -1,1 +1,18 @@
 # TO DO: Write a method which translates a Roman numeral into its integer.
+def roman_style(number)
+    if number.is_a? String
+        letters=number.chars
+
+    else
+        return "error"
+    end   
+ end
+
+
+if condition?
+    # If condition is true
+elsif condition2?
+    # If condition is false but condition2 is true
+else
+    # If both condiitons are false
+end
--- 03-Advanced-ruby/04-Reverse-roman-style/spec/reverse_roman_style_spec.rb
+++ 03-Advanced-ruby/04-Reverse-roman-style/spec/reverse_roman_style_spec.rb
@@ -1,1 +1,19 @@
-# TO DO: Write specs for your reverse_roman_style method
+# TO DO: Write specs for your reverse_reverse_roman_style method
+require 'reverse_reverse_roman_style'
+
+describe '#reverse_roman_style' do
+    it 'returns an integer in the converted Roman numeral' do
+        expect(reverse_roman_style('XVII')).to eq(17)
+        expect(reverse_roman_style('LVI')).to eq(56)
+        expect(reverse_roman_style('DXLV')).to eq(545)
+        expect(reverse_roman_style('MCMXCVIII')).to eq(1998)
+        expect(reverse_roman_style('MMXXII')).to eq(2022)
+
+    end
+
+    it "return an error when you don't pass a string" do
+        expect(reverse_roman_style(3.14)).to eq('error')
+    end
+end
+
+
